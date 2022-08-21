915fdeed62e57eb3647b7eb96bd96acc5abbf55e
Soila Torome <soila.torome@student.moringaschool.com>
2022-08-21 19:46:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 62;
+let num2 = 1;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 34;
+let num4 = 5;
+
+const mod = num3%num4;
+
+let max = Math.max(0,5,10,15,20);
